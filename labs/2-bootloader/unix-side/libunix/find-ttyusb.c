1e4f6f1316bfce64911b3ccee3059b3219bf9657
Lieu Zheng Hong <lieu@lieuzhenghong.com>
2020-12-14 00:49:54 +0800

    Add attempt for find-ttyusb.c

--- labs/2-bootloader/unix-side/libunix/find-ttyusb.c
+++ labs/2-bootloader/unix-side/libunix/find-ttyusb.c
@@ -16,7 +16,14 @@ static const char *ttyusb_prefixes[] = {
 static int filter(const struct dirent *d) {
     // scan through the prefixes, returning 1 when you find a match.
     // 0 if there is no match.
-    unimplemented();
+    const char* file_name = d->d_name;
+    for (int i = 0; i < 2; i++) {
+        const char* prefix = ttyusb_prefixes[i];
+        if (strstr(file_name, prefix) != NULL) {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 // find the TTY-usb device (if any) by using <scandir> to search for
@@ -30,7 +37,21 @@ char *find_ttyusb(void) {
 
     // use <alphasort> in <scandir>
     // return a malloc'd name so doesn't corrupt.
-    unimplemented();
+    struct dirent **fileListTemp;
 
+    int num_dirs = scandir("/dev", &fileListTemp, filter, alphasort);
+
+    if (num_dirs == 0) {
+        perror("no file found");
+        exit(1);
+    } else if (num_dirs > 1) {
+        perror("to many file found");
+        exit(1);
+    }
+    p = (char*)malloc(strlen(fileListTemp[0]->d_name)+1);
+    strcpy(p, fileListTemp[0]->d_name);
+
+    free(fileListTemp[0]);
+    free(fileListTemp);
     return p;
 }
